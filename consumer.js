f733ca93ff997021dfcfd435cb8621010f56e145
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-10-26 21:08:00 +0530

    add comments

--- consumer.js
+++ consumer.js
@@ -4,7 +4,9 @@ import amqp from "amqplib";
 connect();
 async function connect() {
   try {
+    // create a connection
     const connection = await amqp.connect("amqp://localhost:5672");
+    // create a channel
     const channel = await connection.createChannel();
     await channel.assertQueue("jobQ");
     channel.consume("jobQ", (message) => {
